3142e244b2b1eb9a4795777594b55d8f72d71adb
antoniorusso2 <russoantonio303@outlook.it>
2024-12-12 12:55:15 +0100

    - add header and searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './components/Header';
+import { GlobalContext } from './globalContext';
 
 function App() {
   return (
-    <>
-      <div>boolflix</div>
-    </>
+    <GlobalContext.Provider>
+      <Header />
+    </GlobalContext.Provider>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <nav>
+          <div className="serchbar">
+            <input type="text" />
+            <button className="btn search">Cerca</button>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}

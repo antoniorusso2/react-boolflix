1dbd175525821d25ba323ce9ee28366a495a36c5
antoniorusso2 <russoantonio303@outlook.it>
2024-12-13 16:34:40 +0100

    - implmented star for media ratings

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,47 @@
 /* eslint-disable react/prop-types */
+//fontawesome icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons';
+import { faStar as filledStar } from '@fortawesome/free-solid-svg-icons';
 import flags from '../utilities/flags';
+import placeholder from '../assets/placeholder.jpg';
 
 const BASE_IMG_URI = 'https://image.tmdb.org/t/p/';
 
 export default function Card({ item }) {
   const { title, original_title, original_language, vote_average, backdrop_path } = item;
+  const imgPath = `${BASE_IMG_URI}w342${backdrop_path}`;
+
+  const vote = Math.ceil(vote_average / 2);
+  const maxVote = 5;
+
+  const numOfStars = [];
+
+  for (let i = 0; i < maxVote; i++) {
+    i < vote ? numOfStars.push(i + 1) : numOfStars.push('');
+  }
+
   return (
     <div className="card">
       <div className="card-thumb">
-        <img src={`${BASE_IMG_URI}w342${backdrop_path}`} alt="" />
-        poster film
+        <h3>{title}</h3>
+        <img src={imgPath || placeholder} alt="" />
       </div>
 
       <div className="overlay details">
-        <h3>{title}</h3>
         <p className="title">Titolo: {title}</p>
         <p className="original-title">Titolo originale: {original_title}</p>
         <div className="language">
           <span>Lingua:</span> <img className="flag" src={flags[original_language]} alt="" />
         </div>
-        <p className="ratings">Voto {vote_average}</p>
+        <p className="ratings">
+          Voto {vote}
+          <div className="stars">
+            {numOfStars.map((star, index) => (
+              <span key={index}>{star ? <FontAwesomeIcon icon={filledStar} /> : <FontAwesomeIcon icon={emptyStar} />}</span>
+            ))}
+          </div>
+        </p>
       </div>
     </div>
   );

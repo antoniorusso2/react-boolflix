abd12430986efb709990cdc9a037dee289e4cce3
antoniorusso2 <russoantonio303@outlook.it>
2024-12-13 12:05:46 +0100

    - thumbnail image path

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,16 @@
 /* eslint-disable react/prop-types */
 import flags from '../utilities/flags';
 
+const BASE_IMG_URI = 'https://image.tmdb.org/t/p/';
+
 export default function Card({ item }) {
-  const { title, original_title, original_language, vote_average } = item;
+  const { title, original_title, original_language, vote_average, backdrop_path } = item;
   return (
     <div className="card">
-      <div className="card-thumb">poster film</div>
+      <div className="card-thumb">
+        <img src={`${BASE_IMG_URI}w342${backdrop_path}`} alt="" />
+        poster film
+      </div>
 
       <div className="overlay details">
         <h3>{title}</h3>
